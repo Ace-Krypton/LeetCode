63d08372754096052b3b9246c47efde8b067da9e
Ace-Krypton <ramizna@code.edu.az>
2023-08-24 20:24:53 +0200

    Removed Unused Predicate

--- Easy/MergeSortedArray/include/solution.hpp
+++ Easy/MergeSortedArray/include/solution.hpp
@@ -10,10 +10,8 @@ class Solution {
 public:
     static auto merge(std::vector<int32_t> &first, int32_t m,
                       std::vector<int32_t> &second, int32_t n) -> void {
-        first.erase(std::remove_if(first.begin() + m, first.end(),
-                                   [](const int32_t&) -> bool { return true; }), first.end());
-        second.erase(std::remove_if(second.begin() + n, second.end(),
-                                   [](const int32_t&) -> bool { return true; }), second.end());
+        first.erase(first.begin() + m, first.end());
+        second.erase(second.begin() + n, second.end());
         first.insert(first.end(), second.begin(), second.end());
         std::ranges::sort(first);
         std::ranges::copy(first, std::ostream_iterator<char>(std::cout, " "));
