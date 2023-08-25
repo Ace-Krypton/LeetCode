4609a086d85175b958c18b25b92366e029da21a0
Ace-Krypton <ramizna@code.edu.az>
2023-08-25 23:38:18 +0200

    Added Jump Game

---
+++ Medium/JumpGame/include/solution.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+#include <ranges>
+#include <numeric>
+#include <iostream>
+#include <algorithm>
+#include <gtest/gtest.h>
+
+class Solution {
+public:
+    static auto can_jump(std::vector<int32_t> &nums) -> bool {
+
+    }
+};
---
+++ Medium/JumpGame/tests/test.cpp
@@ -0,0 +1,29 @@
+#include "../include/solution.hpp"
+
+class JumpGame : public ::testing::Test {
+protected:
+    ~JumpGame() override = default;
+};
+
+TEST_F(JumpGame, FirstTest) {
+    std::vector<int32_t> elements = {2, 3, 1, 1, 4};
+
+    bool result = Solution::can_jump(elements);
+    bool expected = true;
+
+    ASSERT_EQ(result, expected);
+}
+
+TEST_F(JumpGame, SecondTest) {
+    std::vector<int32_t> elements = {3, 2, 1, 0, 4};
+
+    bool result = Solution::can_jump(elements);
+    bool expected = false;
+
+    ASSERT_EQ(result, expected);
+}
+
+auto main(int argc, char **argv) -> int {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
