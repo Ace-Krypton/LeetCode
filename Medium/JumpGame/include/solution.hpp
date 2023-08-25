29a51fa1ef7f975e3f81b7ce6da4d7d2fa4f5fe6
Ace-Krypton <ramizna@code.edu.az>
2023-08-26 00:19:15 +0200

    Added Jump Game

--- Medium/JumpGame/include/solution.hpp
+++ Medium/JumpGame/include/solution.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
 #include <vector>
-#include <ranges>
-#include <numeric>
 #include <iostream>
 #include <algorithm>
 #include <gtest/gtest.h>
@@ -10,6 +8,19 @@
 class Solution {
 public:
     static auto can_jump(std::vector<int32_t> &nums) -> bool {
+        if (nums.front() == 0 && nums.size() != 1) return false;
 
+        if (!(std::ranges::find(nums, 0) != nums.end())) {
+            return true;
+        }
+
+        int32_t reachable = 0;
+
+        for (int32_t i = 0; i < nums.size(); ++i) {
+            if (reachable < i) return false;
+            reachable = std::max(reachable, i + nums[i]);
+        }
+
+        return true;
     }
 };
