e3a38abd76f6f3716cb18cd54fb5120d2f189eae
Ace-Krypton <ramizna@code.edu.az>
2023-09-01 00:51:18 +0200

    Added Jewels And Stones

--- Easy/JewelsAndStones/include/solution.hpp
+++ Easy/JewelsAndStones/include/solution.hpp
@@ -9,8 +9,8 @@ class Solution {
 public:
     static auto num_jewels_in_stones(const std::string &jewels,
                                      const std::string &stones) -> std::size_t {
-        return std::ranges::count_if(stones,[&](const char &stone) -> bool {
-            return std::ranges::any_of(jewels,[&](const char &jewel) -> bool {
+        return std::ranges::count_if(stones, [&](const char &stone) -> bool {
+            return std::ranges::any_of(jewels, [&](const char &jewel) -> bool {
                 return jewel == stone;
             });
         });
