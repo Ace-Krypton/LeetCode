d32cea539a2ff1268c27ea95d679c45ab5f3e825
Ace-Krypton <ramizna@code.edu.az>
2023-08-31 23:55:33 +0200

    Added Valid Palindrome

---
+++ Easy/ValidPalindrome/include/solution.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <gtest/gtest.h>
+
+class Solution {
+public:
+    static auto is_palindrome(std::string &word) -> bool {
+        std::transform(word.begin(), word.end(), word.begin(),
+                       [](const unsigned char &c) -> int32_t {
+            return std::tolower(c);
+        });
+
+        word.erase(std::remove_if(word.begin(), word.end(),
+                               [](const char &c) -> bool {
+            return !std::isalnum(c);
+        } ), word.end());
+
+        if (word.length() % 2 == 1) {
+            for (std::size_t i = word.length() / 2,
+                    j = word.length() / 2; i != 0; --i, ++j) {
+                if (word[i] != word[j]) return false;
+            }
+        } else {
+            for (std::size_t i = word.length() / 2 - 1,
+                    j = word.length() / 2; i != -1; --i, ++j) {
+                if (word[i] != word[j]) return false;
+            }
+        }
+
+        return true;
+    }
+};
---
+++ Easy/ValidPalindrome/tests/test.cpp
@@ -0,0 +1,38 @@
+#include "../include/solution.hpp"
+
+class ValidPalindrome : public ::testing::Test {
+protected:
+    ~ValidPalindrome() override = default;
+};
+
+TEST_F(ValidPalindrome, FirstTest) {
+    std::string sentence = "A man, a plan, a canal: Panama";
+
+    bool result = Solution::is_palindrome(sentence);
+    bool expected = true;
+
+    ASSERT_EQ(result, expected);
+}
+
+TEST_F(ValidPalindrome, SecondTest) {
+    std::string sentence = "race a car";
+
+    bool result = Solution::is_palindrome(sentence);
+    bool expected = false;
+
+    ASSERT_EQ(result, expected);
+}
+
+TEST_F(ValidPalindrome, ThirdTest) {
+    std::string sentence = " ";
+
+    bool result = Solution::is_palindrome(sentence);
+    bool expected = true;
+
+    ASSERT_EQ(result, expected);
+}
+
+auto main(int argc, char **argv) -> int {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
