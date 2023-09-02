3437b1d824a40cbf72d80998aa2b80d2be5228d9
Ace-Krypton <ramizna@code.edu.az>
2023-09-02 21:52:34 +0200

    Added Three Sum

--- Medium/ThreeSum/include/solution.hpp
+++ Medium/ThreeSum/include/solution.hpp
@@ -9,6 +9,28 @@ class Solution {
 public:
     static auto three_sum(std::vector<int32_t> &nums)
         -> std::vector <std::vector<int32_t>> {
-        
+        std::ranges::sort(nums);
+        std::set<std::vector<int32_t>> unique_rows;
+
+        for (std::size_t i = 0; i < nums.size() - 2; ++i) {
+            std::size_t left = i + 1;
+            std::size_t right = nums.size() - 1;
+
+            while (left < right) {
+                int32_t sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) unique_rows.insert({nums[i],
+                                                  nums[left],
+                                                  nums[right]}),
+                                                  ++left, --right;
+                else if (sum < 0) ++left;
+                else --right;
+            }
+        }
+
+        std::vector<std::vector<int32_t>> result(unique_rows.begin(),
+                                                 unique_rows.end());
+
+        return result;
     }
 };
