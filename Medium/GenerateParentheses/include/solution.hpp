5e742a9e1093de78b1407364d55edb3ced8f32a4
Ace-Krypton <ramizna@code.edu.az>
2023-09-03 22:58:52 +0200

    Added Generate Parenthesis

--- Medium/GenerateParentheses/include/solution.hpp
+++ Medium/GenerateParentheses/include/solution.hpp
@@ -3,11 +3,27 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include <functional>
 #include <gtest/gtest.h>
 
 class Solution {
 public:
     static auto generate_parenthesis(const int32_t n) -> std::vector<std::string> {
-        
+        std::vector<std::string> result;
+
+        std::function<void(std::string, int32_t, int32_t)> backtrack = [&](const std::string &current,
+                const int32_t open, const int32_t close) -> void {
+            if (current.length() == n * 2) {
+                result.emplace_back(current);
+                return;
+            } if (open < n) {
+                backtrack(current + '(', open + 1, close);
+            } if (close < open) {
+                backtrack(current + ')', open, close + 1);
+            }
+        };
+
+        backtrack({ }, 0, 0);
+        return result;
     }
 };
