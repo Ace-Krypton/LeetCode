97fcdc544256088aced82703be58f1362676e619
Ace-Krypton <ramizna@code.edu.az>
2023-09-03 21:39:17 +0200

    Added Longest SubString Without Repeating Characters

--- Medium/LongestSubstringWithoutRepeatingCharacters/include/solution.hpp
+++ Medium/LongestSubstringWithoutRepeatingCharacters/include/solution.hpp
@@ -7,13 +7,14 @@
 
 class Solution {
 public:
-    auto static length_of_longest_substring(const std::string &str) -> std::size_t {
+    static auto length_of_longest_substring(const std::string &str) -> std::size_t {
         std::size_t start = 0;
         std::size_t max_len = 0;
         std::unordered_map<char, std::size_t> substring;
 
         for (std::size_t i = 0; i < str.size(); ++i) {
-            if (substring.count(str[i]) && substring[str[i]] >= start) {
+            if (substring.count(str[i])
+                && substring[str[i]] >= start) {
                 start = substring[str[i]] + 1;
             }
 
