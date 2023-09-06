17d4e61c443bbf8a9bae04b61cf6914b26a13e6c
Ace-Krypton <ramizna@code.edu.az>
2023-09-06 22:49:54 +0200

    Added Get Combinations

--- Easy/GetCombinations/include/solution.hpp
+++ Easy/GetCombinations/include/solution.hpp
@@ -10,8 +10,8 @@ public:
     static auto get_combinations(std::vector<int32_t> &combinations) -> std::size_t {
         std::size_t count = 0;
 
-        for (int32_t i = 0; i < combinations.size(); ++i) {
-            for (int32_t j = i + 1; j < combinations.size(); ++j) {
+        for (std::size_t i = 0; i < combinations.size(); ++i) {
+            for (std::size_t j = i + 1; j < combinations.size(); ++j) {
                 if (combinations[i] + combinations[j] <= 100) {
                     ++count;
                 }
