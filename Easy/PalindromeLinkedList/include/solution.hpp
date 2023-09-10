7071f8309c614adf19741aa26bce28061dcdb403
Ace-Krypton <ramizna@code.edu.az>
2023-09-10 17:18:43 +0200

    Added Palindrome Linked List

--- Easy/PalindromeLinkedList/include/solution.hpp
+++ Easy/PalindromeLinkedList/include/solution.hpp
@@ -1,8 +1,49 @@
-//
-// Created by draco on 9/12/23.
-//
+#pragma once
 
-#ifndef LEETCODEJOURNEY_SOLUTION_HPP
-#define LEETCODEJOURNEY_SOLUTION_HPP
+#include <ranges>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <gtest/gtest.h>
 
-#endif //LEETCODEJOURNEY_SOLUTION_HPP
+struct ListNode {
+    int32_t value;
+    ListNode *next;
+};
+
+class Solution {
+public:
+    static auto is_palindrome(ListNode *head) -> bool {
+        std::vector<int32_t> numbers;
+
+        while (head != nullptr) {
+            numbers.emplace_back(head->value);
+            head = head->next;
+        }
+
+        std::size_t left = 0;
+        std::size_t right = numbers.size() - 1;
+
+        while (left < right) {
+            if (numbers[left] != numbers[right]) {
+                return false;
+            } ++left, --right;
+        }
+
+        return true;
+    }
+
+    static auto insert(ListNode **head_ref, int32_t new_value) -> void {
+        auto *node = new ListNode();
+        node->value = new_value;
+        node->next = (*head_ref);
+        (*head_ref) = node;
+    }
+
+    static auto print(ListNode *head) -> void {
+        while (head != nullptr) {
+            std::cout << head->value << ' ';
+            head = head->next;
+        }
+    }
+};
--- Easy/PalindromeLinkedList/tests/test.cpp
+++ Easy/PalindromeLinkedList/tests/test.cpp
@@ -1,3 +1,13 @@
-//
-// Created by draco on 9/12/23.
-//
+#include "../include/solution.hpp"
+
+auto main() -> int {
+    ListNode *head = nullptr;
+    Solution::insert(&head, 1);
+    Solution::insert(&head, 2);
+    Solution::insert(&head, 2);
+    Solution::insert(&head, 1);
+
+    std::cout << std::boolalpha << Solution::is_palindrome(head);
+
+    return 0;
+}
