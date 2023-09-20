2718071cc6450e78053f54fbb3155abd568557f5
Ace-Krypton <ramizna@code.edu.az>
2023-09-20 22:45:51 +0200

    Added Count Complete Tree Nodes logNxlogN

--- Easy/CountCompleteTreeNodes/include/solution.hpp
+++ Easy/CountCompleteTreeNodes/include/solution.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cmath>
 #include <vector>
 #include <iostream>
 #include <algorithm>
@@ -16,16 +17,41 @@ struct TreeNode {
 class Solution {
 public:
     static auto count_nodes(TreeNode *root) -> int32_t {
+        if (!root) return 0;
+
+        std::function<int32_t(TreeNode*)> lh =
+                [&](TreeNode *root) -> int32_t {
+                    if (!root) return 0;
+                    return lh(root->left) + 1;
+                };
+
+        std::function<int32_t(TreeNode*)> rh =
+                [&](TreeNode *root) -> int32_t {
+                    if (!root) return 0;
+                    return rh(root->right) + 1;
+                };
+
+        const int32_t left_height = lh(root);
+        const int32_t right_height = rh(root);
+
+        if (left_height == right_height) {
+            return std::pow(2, left_height) - 1;
+        }
+
+        return count_nodes(root->left) + count_nodes(root->right) + 1;
+    }
+
+    [[maybe_unused]] static auto count_nodes_tle(TreeNode *root) -> int32_t {
         if (root == nullptr) return 0;
 
         int32_t count = 0;
         std::function<void(TreeNode*)> preorder =
                 [&](TreeNode *root) -> void {
-            if (root == nullptr) return;
-            ++count;
-            preorder(root->left);
-            preorder(root->right);
-        };
+                    if (root == nullptr) return;
+                    ++count;
+                    preorder(root->left);
+                    preorder(root->right);
+                };
 
         preorder(root);
         return count;
