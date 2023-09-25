aee727e1567c197dc25d5e9a3f171c2161e84c5a
Ace-Krypton <ramizna@code.edu.az>
2023-09-25 21:36:28 +0200

    Added Product Except Self

--- Medium/ProductOfArrayExceptSelf/include/solution.hpp
+++ Medium/ProductOfArrayExceptSelf/include/solution.hpp
@@ -27,8 +27,7 @@ public:
         return output;
     }
 
-    static auto product_except_self(
-            std::vector<int32_t> &nums) -> std::vector<int32_t> {
+    static auto product_except_self_j(std::vector<int32_t> &nums) -> std::vector<int32_t> {
         std::vector<int32_t> output(nums.size(), 1);
 
         int32_t left_product = 1;
@@ -44,5 +43,46 @@ public:
         }
 
         return output;
+    }
+
+    static auto product_except_self(std::vector<int32_t> &nums) -> std::vector<int32_t> {
+        std::vector<int32_t> output(nums.size());
+        std::vector<int32_t> left_products(nums.size());
+        std::vector<int32_t> right_products(nums.size());
+        left_products[0] = 1;
+        right_products[nums.size() - 1] = 1;
+
+        for (size_t i = 1; i < nums.size(); ++i) {
+            left_products[i] = nums[i - 1] * left_products[i - 1];
+        } for (size_t i = nums.size() - 2; i > 0; --i) {
+            right_products[i] = nums[i + 1] * right_products[i + 1];
+        }
+        right_products[0] = nums[1] * right_products[1];
+        for (size_t i = 0; i < nums.size(); ++i) {
+            output[i] = left_products[i] * right_products[i];
+        }
+
+        return output;
+    }
+
+    [[maybe_unused]] static auto product_except_self_my_way(
+            std::vector<int32_t> &nums) -> std::vector<int32_t> {
+        size_t zero_count = std::count(nums.begin(), nums.end(), 0);
+        int32_t multi = std::accumulate(std::begin(nums),
+                                        std::end(nums), 1,
+                                        std::multiplies<>());
+
+        std::vector<int32_t> result(nums.size(), 0);
+        if (zero_count >= 2) return result;
+
+        for (size_t i = 0; i < nums.size(); ++i) {
+            if (zero_count == 1 && nums[i] == 0) {
+                result[i] = multi;
+            } else if (nums[i] != 0) {
+                result[i] = zero_count > 0 ? 0 : multi / nums[i];
+            }
+        }
+
+        return result;
     }
 };
